e8299a0d4a280b2338488346865a464dc639c54e
DELL <binsiyanazer21@gmail.com>
2024-05-24 16:10:59 +0530

    new

--- lib/models/addressModel.dart
+++ lib/models/addressModel.dart
@@ -1,21 +1,22 @@
 class addressModel{
-  String? address,name,number,landmark,houseno,pincode;
+  String? address,name,number,location,pincode,deliveryinsruction;
   addressModel({
     required this.name,
     required this.number,
     required this.address,
-    required this.landmark,
-    required this.houseno,
-    required this.pincode});
+    required this.location,
+    required this.pincode,
+    required this.deliveryinsruction
+  });
 
   Map <String, dynamic> toMap(){
     return{
       "address" : this.address,
       "name" : this.name,
       "number" : this.number,
-      "landmark" : this.landmark,
-      "houseno" : this.houseno,
+      "location" : this.location,
       "pincode" : this.pincode,
+      "deliveryinsruction":this.deliveryinsruction
     };
   }
   factory addressModel.fromMap (Map <String, dynamic> map){
@@ -23,21 +24,21 @@ class addressModel{
         address : map["address"] ?? "",
         name : map["name"] ?? "",
         number : map["number"]??"",
-        landmark : map["landmark"] ?? "",
-        houseno: map["houseno"] ?? "",
-        pincode: map["pincode"]??""
+        location : map["location"] ?? "",
+        pincode: map["pincode"]??"",
+        deliveryinsruction: map["deliveryinsruction"]??""
     );
   }
   addressModel copyWith({
-    String? address,name,number,landmark,houseno,pincode
+    String? address,name,number,landmark,houseno,pincode,deliveryinsruction
   }){
     return addressModel(
       address: address ?? this.address,
       name: name ?? this.name,
       number: number ?? this.number,
-      landmark: landmark ?? this.landmark,
-      houseno: houseno ?? this.houseno,
+      location: location ?? this.location,
       pincode: pincode ?? this.pincode,
+      deliveryinsruction:  deliveryinsruction ?? this.deliveryinsruction,
     );
   }
 
